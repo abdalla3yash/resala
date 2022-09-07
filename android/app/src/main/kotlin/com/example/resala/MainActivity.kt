4f1cb09414345100a8bf4fb9d9e301bf995496ef
abdalla3yash <abdo3yash@gmail.com>
2022-09-07 16:18:45 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/resala/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.resala
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/widget/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color mainRedColor = Color(0xFFe53935);
+  static const Color mainBlueColor = Color(0xFF3f51b5);
+}
