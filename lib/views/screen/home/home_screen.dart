318d4e82068313b68bf08c7105df77d8ec34160d
abdalla3yash <abdo3yash@gmail.com>
2022-09-11 13:24:36 +0200

    finish CarouselSlider homing

--- lib/views/screen/home/home_screen.dart
+++ lib/views/screen/home/home_screen.dart
@@ -1,19 +1,21 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:resala/controller/controllers/auth_controller.dart';
+import 'package:resala/controller/controllers/home_controller.dart';
 import 'package:resala/model/routes/router.dart';
 import 'package:resala/views/widget/colors.dart';
 import 'package:resala/views/widget/custom_loader.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.mainRedColor,
-        title: Text(
+        title: const Text(
           "رساله",
           style: TextStyle(fontSize: 20),
         ),
@@ -33,8 +35,71 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       backgroundColor: AppColors.whiteColor,
-      body: Center(
-        child: Text("الصفحه الرئيسيه تحت الانشاء"),
+      body: GetBuilder<HomeController>(
+        builder: ((homeController) {
+          return homeController.isLoaded
+              ? Column(
+                  children: [
+                    CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        aspectRatio: 1.8,
+                        enlargeCenterPage: true,
+                      ),
+                      items: homeController.homeImageList
+                          .map(
+                            (item) => Container(
+                              margin: const EdgeInsets.only(
+                                top: 10,
+                                left: 5,
+                                right: 5,
+                                bottom: 10,
+                              ),
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(
+                                    15.0,
+                                  ),
+                                ),
+                                child: Stack(
+                                  children: <Widget>[
+                                    Image.network(
+                                      item,
+                                      fit: BoxFit.cover,
+                                      width: 1000.0,
+                                    ),
+                                    Positioned(
+                                      bottom: 0.0,
+                                      left: 0.0,
+                                      right: 0.0,
+                                      child: Container(
+                                        decoration: const BoxDecoration(
+                                          gradient: LinearGradient(
+                                            colors: [
+                                              Color.fromARGB(200, 0, 0, 0),
+                                              Color.fromARGB(0, 0, 0, 0)
+                                            ],
+                                            begin: Alignment.bottomCenter,
+                                            end: Alignment.topCenter,
+                                          ),
+                                        ),
+                                        padding: const EdgeInsets.symmetric(
+                                          vertical: 10.0,
+                                          horizontal: 20.0,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ],
+                )
+              : CustomLoader();
+        }),
       ),
     );
   }
